8e02754ae7ed97320bce90d9ac8d84902122bf61
ninetycode <mattez19@live.com>
2026-03-18 01:45:14 -0300

    object pull : bullets
    ahora las balas son un objetc pull, ayuda a que no si se instancian 800 balas no haya problemas de rendimiento. DATO: he creado otras capas de colision, el jugador es el 4. ahora tambien hay enemigos y balas.

--- Entities/Player/weapon_component.gd
+++ Entities/Player/weapon_component.gd
@@ -6,36 +6,21 @@ const BULLET_SCENE = preload("res://Scenes/Bullet.tscn")
 @onready var cooldown_timer = $CooldownTimer
 @onready var player = owner
 
-func disparar(direccion_fija: Vector2 = Vector2.ZERO):
-	if not cooldown_timer.is_stopped():
-		return
-
-	# Si no nos pasan dirección, la calculamos
-	var final_dir = direccion_fija
-	if final_dir == Vector2.ZERO:
-		final_dir = obtener_direccion_apuntado()
-
-	var bullet = BULLET_SCENE.instantiate()
-	
-	# USAMOS EL NODO MUZZLE QUE VEO EN TU CAPTURA
-	# Está adentro de AnimatedSprite2D
-	bullet.global_position = player.get_node("AnimatedSprite2D/muzzle").global_position
-	
-	bullet.direction = final_dir
-	bullet.rotation = final_dir.angle()
-	
-	get_tree().current_scene.add_child(bullet)
-	cooldown_timer.start()
+func disparar():
+	if cooldown_timer.is_stopped():
+		var direccion = obtener_direccion_apuntado()
+		
+		# Le pasamos la pelota al Autoload
+		BulletPool.disparar_bala(player.muzzle.global_position, direccion)
+		
+		cooldown_timer.start()
 
 func obtener_direccion_apuntado() -> Vector2:
-	var h = Input.get_axis("ui_left", "ui_right")
 	var v = Input.get_axis("ui_up", "ui_down")
-	
-	if owner.is_on_floor() and v > 0: v = 0
-	
+	var h = Input.get_axis("ui_left", "ui_right")
 	var dir = Vector2(h, v)
 	
-	if dir.length() < 0.1:
-		return Vector2.LEFT if owner.get_node("AnimatedSprite2D").flip_h else Vector2.RIGHT
+	if dir == Vector2.ZERO:
+		return Vector2.LEFT if player._animated_sprite.flip_h else Vector2.RIGHT
 		
 	return dir.normalized()
