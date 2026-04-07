7a888a3a326aa5aa7a3a602963d48f16664fbcf2
Facucito-gif <facuscazziotti66@gmail.com>
2026-04-07 04:01:56 -0300

    No hubo avances

--- cinematic_ui.gd
+++ cinematic_ui.gd
@@ -6,8 +6,8 @@ extends CanvasLayer
 func mostrar_barras(duracion: float = 0.5):
 	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC)
 	# Ajustá el 70 según qué tan "cine" quieras que se vea
-	tween.tween_property(top_bar, "custom_minimum_size:y", 70, duracion)
-	tween.tween_property(bottom_bar, "custom_minimum_size:y", 70, duracion)
+	tween.tween_property(top_bar, "custom_minimum_size:y", 40, duracion)
+	tween.tween_property(bottom_bar, "custom_minimum_size:y", 40, duracion)
 
 func ocultar_barras(duracion: float = 0.5):
 	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_CUBIC)
