5008f98e6f43f0a0957519ea8a4de003d2e9879a
Facucito-gif <facuscazziotti66@gmail.com>
2026-04-09 13:14:26 -0300

    Update slide_path.gd

--- Levels/slide_path.gd
+++ Levels/slide_path.gd
@@ -13,30 +13,30 @@
 	#timer.timeout.connect(_on_timer_timeout)
 
 #func iniciar_secuencia():
-	timer.start(tiempo_de_bajada)
+	#timer.start(tiempo_de_bajada)
 	
 	# Encendemos la cinta de correr usando la propiedad nativa de Godot 4.3+
-	parallax_cielo.autoscroll = velocidad_cielo
-	parallax_rampa.autoscroll = velocidad_rampa
+	#parallax_cielo.autoscroll = velocidad_cielo
+	#parallax_rampa.autoscroll = velocidad_rampa
 	
 	# Bloqueamos cámara
-	var camara = get_viewport().get_camera_2d()
-	if camara and camara.has_method("bloquear_camara"):
-		camara.bloquear_camara()
+	#var camara = get_viewport().get_camera_2d()
+	#if camara and camara.has_method("bloquear_camara"):
+		#camara.bloquear_camara()
 		
 	# ¡Forzamos a Blue a entrar en el estado de deslizamiento!
-	var player = get_tree().current_scene.find_child("GunstarBlue", true, false)
-	if player and player.has_node("StateMachine"): # Cambiá "StateMachine" por el nombre de tu nodo
-		player.get_node("StateMachine").transition_to("SlideState")
+	#var player = get_tree().current_scene.find_child("GunstarBlue", true, false)
+	#if player and player.has_node("StateMachine"): # Cambiá "StateMachine" por el nombre de tu nodo
+		#player.get_node("StateMachine").transition_to("SlideState")
 
 #func _on_timer_timeout():
 	# Frenamos los fondos
-	parallax_cielo.autoscroll = Vector2.ZERO
-	parallax_rampa.autoscroll = Vector2.ZERO
+	#parallax_cielo.autoscroll = Vector2.ZERO
+	#parallax_rampa.autoscroll = Vector2.ZERO
 	
-	if muro_inferior:
-		muro_inferior.queue_free()
+	#if muro_inferior:
+		#muro_inferior.queue_free()
 		
-	var camara = get_viewport().get_camera_2d()
-	if camara and camara.has_method("desbloquear_camara"):
-		camara.desbloquear_camara()
+	#var camara = get_viewport().get_camera_2d()
+	#if camara and camara.has_method("desbloquear_camara"):
+		#camara.desbloquear_camara()
